6b4a0b7a4d9715df4c6d1c6504d73d0b4a5a881b
Monster3D <monster3d@mail.ru>
2016-12-18 16:17:10 +0500

    Adds new route for get full client information from api

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,16 @@ $app->get('/', function () use ($app) {
 });
 
 $app->group(['prefix' => 'api'], function() use($app) {
+    
+    /*
+    * Route add new client
+    */
     $app->post('client/add', 'ClientController@add');
+    
+    /*
+    * Route get client info by uid
+    */
+    $app->get('client/info/uid/{uid}', 'ClientController@info');
 });
 
 
