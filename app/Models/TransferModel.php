02947b596742ebeb5390fc2399b1784401190430
Monster3D <monster3d@mail.ru>
2016-12-18 21:33:59 +0500

    Adds Transfer model

---
+++ app/Models/TransferModel.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Models;
+
+class TransferModel extends BaseModel {
+    
+    /**
+    *
+    * From Uid
+    *
+    * @var int
+    *
+    */
+    private $fromUid;
+    
+    /**
+    *
+    * To Uid
+    *
+    * @var int
+    *
+    */
+    private $toUid;
+    
+    /**
+    *
+    * Sum
+    *
+    * @var int
+    *
+    */
+    private $sum;
+
+    public function __construct($data = null)
+    {
+        if ($data !== null) {
+            $this->fromUid = (int)$this->getValue($data, 'from_uid', 0);
+            $this->toUid   = (int)$this->getValue($data, 'to_uid', 0);
+            $this->sum     = (int)$this->getValue($data, 'sum', 0); 
+        }
+    }
+
+    /**
+    *
+    * Get from uid
+    *
+    * @return int
+    *
+    */
+    public function getFromUid()
+    {
+        return $this->fromUid;
+    }
+
+    /**
+    *
+    * Get to uid
+    *
+    * @return int
+    *
+    */
+    public function getToUid()
+    {
+        return $this->toUid;
+    }
+
+    /**
+    *
+    * Get sum
+    *
+    * @return int
+    *
+    */
+    public function getSum()
+    {
+        return $this->sum;
+    }
+
+    /**
+    *
+    * Set from uid
+    *
+    * @param $fromUid
+    *
+    * @return self
+    *
+    */
+    public function setFromUid($fromUid)
+    {
+        $this->fromUid = (int)$fromUid;
+        return $this;
+    }
+
+    /**
+    *
+    * Set to uid
+    *
+    * @param $toUid
+    *
+    * @return self
+    *
+    */
+    public function setToUid($toUid)
+    {
+        $this->toUid = (int)$toUid;
+        return $this;
+    }
+
+    /**
+    *
+    * Set sum
+    *
+    * @param $sum int
+    *
+    * @return self
+    *
+    */
+    public function setSum($sum)
+    {
+        $this->sum = (int)$sum;
+    }
+}
