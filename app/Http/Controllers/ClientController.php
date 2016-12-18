d08e26e00c77eaa448cea7a2b898ada8891ce21c
Monster3D <monster3d@mail.ru>
2016-12-18 16:13:17 +0500

    Adds ability get client information by uid

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -16,6 +16,7 @@ class ClientController extends Controller
     *
     */
     private $storage;
+
     /**
      * Create a new controller instance.
      *
@@ -23,20 +24,75 @@ class ClientController extends Controller
      */
     public function __construct()
     {
-        //
+        //void
     }
 
+    /**
+    *
+    * Contorller add new client
+    *
+    * @param $request Illuminate\Http\Request
+    *
+    * @return array
+    *
+    * @todo Need added validation input data
+    *
+    */
     public function add(Request $request)
     {
 
         $clientModel      = new ClientModel($request->all());
         $clientRepository = new ClientRepository();
-        $clientModel      = $clientRepository->add($clientModel);
+        
+        try {
+            $clientModel      = $clientRepository->add($clientModel);
+        } catch (PDOException $e) {
+            //@todo logger
+        } catch (Exception $e) {
+            //@todo logger
+        }
 
+        unset($clientRepository);
 
         return response()->json(['status' => 0, 'uid' => $clientModel->getUid()], 201);
-
     }
 
-    //
+    /**
+    *
+    * Get client info by uid
+    *
+    * @param $request Illuminate\Http\Request
+    * @param $uid int
+    *
+    * @return array
+    *
+    */
+    public function info(Request $request, $uid)
+    {
+        $clientModel = new ClientModel();
+        $clientModel->setUid($uid);
+        $clientRepository = new ClientRepository();
+        try {
+            $clientModel = $clientRepository->get($clientModel); 
+        } catch (PDOException $e) {
+            //@todo logger
+        } catch (Exception $e) {
+            //@todo logger
+        }
+        
+        $response = [
+            'status' => 0,
+            'data' => [
+                'client' => [
+                    'name'        => $clientModel->getName(),
+                    'country'     => $clientModel->getCountry(),
+                    'city'        => $clientModel->getCity(),
+                    'currency'    => $clientModel->getCurrency(),
+                    'amount'      => $clientModel->getAmount(),
+                    'last_active' => $clientModel->getLastActive()
+               ]
+            ]
+        ];
+        return response()->json($response, 200);
+    }
 }
